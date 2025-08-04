aa0c9f2d45d78d8a2d6901d7d823ae659af3f927
papercoding22 <john.ly997@gmail.com>
2025-08-04 22:58:39 +0700

    feat: enhance HeaderRow component with TypeScript interface and improve styling

--- client/src/examples/calendar/HeaderRow.tsx
+++ client/src/examples/calendar/HeaderRow.tsx
@@ -1,13 +1,18 @@
-const HeaderRow = ({ days }: { days: string[] }) => {
+// HeaderRow Component
+interface HeaderRowProps {
+  days: string[];
+}
+
+const HeaderRow: React.FC<HeaderRowProps> = ({ days }) => {
   return (
-    <div className="grid grid-cols-8 border-b border-b-gray-300">
+    <div className="grid grid-cols-8 border-b border-gray-200">
       {/* Column Time */}
-      <div className="p-4 bg-gray-100 font-medium">Time</div>
+      <div className="p-4 bg-white font-medium"></div>
       {/* 7 Columns of Day */}
       {days.map((day) => (
         <div
           key={day}
-          className="p-4 bg-gray-100 font-medium text-center border-l border-l-gray-300"
+          className="p-4 bg-white font-medium text-center border-l border-gray-200"
         >
           {day}
         </div>
